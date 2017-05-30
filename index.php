b1c86775b93d559129b2777d2f64daf9a4fbce80
OdkoPP <ondrejpit@azet.sk>
2017-05-30 22:58:31 +0200

    Default structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include_once "page_parts/header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            Ready to use PHP
+        </div>
+    </div>
+</div>
+
+<?php
+include_once "page_parts/footer.php";
+?>
---
+++ page_parts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="asd"/>
+    <meta name="keywords" content="asd"/>
+    <meta name="author" content="asd"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Title</title>
+
+    <!-- jQuery.js -->
+    <script src="/include/jquery.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/include/bootstrap/css/bootstrap.min.css">
+    <script src="/include/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
